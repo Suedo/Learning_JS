359387389fcae2eda09efa00f4462ce83d2ed0d9
Suedo <somjitnag192@gmail.com>
2018-11-29 07:34:54 +0530

    object destructuring 101

--- ES6 KyleSimpson/destructuring_.js
+++ ES6 KyleSimpson/destructuring_.js
@@ -3,12 +3,32 @@
 function returnMix() {
   return [1, 2, 3, [4, 5, 6]];
 }
+function returnObject() {
+  return { p:2, q:3, r:4};
+}
 
 var a, b, c, d, e, collect;
 
-[, , c, [d, e] ] = 
+[, , c, 
+    [d, e]
+] = 
 [a, b, ...collect] = 
 returnMix();
 console.log(collect); // [ 3, [ 4, 5, 6 ] ]
 console.log(c, d, e); // 3 4 5
 
+
+/**
+ * Object destructuring
+ */
+
+var {
+  p,
+  q: X, // value of q from returned object goes to X
+  r,
+  s: Y = 50
+} = returnObject()
+
+console.log(JSON.stringify(returnObject(),null,2));
+
+console.log("value of X and Y :",X,Y);
